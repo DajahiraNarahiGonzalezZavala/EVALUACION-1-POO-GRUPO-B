66f377ebba591ea9fb5c3a28ba0da3d5990e9d5e
DajahiraNarahiGonzalezZavala <101366240+DajahiraNarahiGonzalezZavala@users.noreply.github.com>
2022-03-12 02:30:55 -0800

    Add files via upload

---
+++ EVA1_11_CLASES/src/eva1_11_clases/EVA1_11_CLASES.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_11_clases;
+
+/**
+ *
+ * @author Narahi Zavala
+ */
+public class EVA1_11_CLASES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
+class Persona {
+       private String nombre;
+     private String id;
+     private int edad;
+     //this/ este apuntador al objeto actual apuntador
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+     
+}
