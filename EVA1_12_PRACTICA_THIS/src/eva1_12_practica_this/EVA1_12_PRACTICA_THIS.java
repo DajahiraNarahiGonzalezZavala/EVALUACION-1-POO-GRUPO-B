473786342c91eb8b99c7d33798f5735a561d6410
DajahiraNarahiGonzalezZavala <101366240+DajahiraNarahiGonzalezZavala@users.noreply.github.com>
2022-03-12 02:31:18 -0800

    Add files via upload

---
+++ EVA1_12_PRACTICA_THIS/src/eva1_12_practica_this/EVA1_12_PRACTICA_THIS.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_12_practica_this;
+
+/**
+ *
+ * @author Narahi Zavala
+ */
+public class EVA1_12_PRACTICA_THIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+ class Automovil {
+     private String marca;
+     private String modelo;
+     private int año;
+     private double precio;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+     
+ }
